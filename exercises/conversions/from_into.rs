215a3b0bc7bf4e09427379e76d7a0d0204585d01
Jason Yoo <jasonyoo0515@gmail.com>
2022-12-04 19:46:31 +0900

    done: conversions

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -3,6 +3,8 @@
 // You can read more about it at https://doc.rust-lang.org/std/convert/trait.From.html
 // Execute `rustlings hint from_into` or use the `hint` watch subcommand for a hint.
 
+use std::num::ParseIntError;
+
 #[derive(Debug)]
 struct Person {
     name: String,
@@ -35,10 +37,28 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+
+        let split_str = s.split(",").collect::<Vec<&str>>();
+        if split_str.len() != 2 {
+            return Person::default();
+        }
+        if split_str[0].len() == 0 {
+            return Person::default();
+        }
+
+        let parse_age_result = split_str[1].parse::<usize>();
+        match parse_age_result {
+            Ok(age) => Person {
+                name: split_str[0].to_string(),
+                age: age,
+            },
+            ParseIntError => Person::default(),
+        }
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -28,8 +28,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -46,6 +44,28 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err(ParsePersonError::Empty);
+        }
+
+        let split_str = s.split(",").collect::<Vec<&str>>();
+        if split_str.len() != 2 {
+            return Err(ParsePersonError::BadLen);
+        }
+
+        let name = split_str[0].to_string();
+        if name.len() == 0 {
+            return Err(ParsePersonError::NoName);
+        }
+
+        let parse_age_result = split_str[1].parse::<usize>();
+        match parse_age_result {
+            Ok(age) => Ok(Person {
+                name: name,
+                age: age,
+            }),
+            Err(ParseIntError) => Err(ParsePersonError::ParseInt(ParseIntError)),
+        }
     }
 }
 
